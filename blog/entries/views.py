a8dc260f1971928eb0774d3550ae8371e19a9042
Govindakumar181 <Govindakumar181@gmail.com>
2021-01-19 10:29:36 +0500

    Views, Urls, and Templates added

--- blog/entries/views.py
+++ blog/entries/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Entry
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'entries/index.html')
